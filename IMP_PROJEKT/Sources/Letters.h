c9fc19989e4a481d20d9c7bab3b2980151d3e93c
Denis Novosád <60551739+PendasDreams@users.noreply.github.com>
2023-11-20 07:46:46 +0100

    FINAL

---
+++ IMP_PROJEKT/Sources/Letters.h
@@ -0,0 +1,49 @@
+#ifndef __LETTERS_H__
+#define __LETTERS_H__
+
+#define HEIGHT 8
+#define WIDTH 8
+#define MAX 64
+
+
+void fillField(int fieldLength, char field[HEIGHT][MAX], char template[HEIGHT][WIDTH]);
+
+void add_Space(char field[HEIGHT][MAX], int curr_field_length);
+void add_A(char field[HEIGHT][MAX], int curr_field_length);
+void add_B(char field[HEIGHT][MAX], int curr_field_length);
+void add_C(char field[HEIGHT][MAX], int curr_field_length);
+void add_D(char field[HEIGHT][MAX], int curr_field_length);
+void add_E(char field[HEIGHT][MAX], int curr_field_length);
+void add_F(char field[HEIGHT][MAX], int curr_field_length);
+void add_G(char field[HEIGHT][MAX], int curr_field_length);
+void add_H(char field[HEIGHT][MAX], int curr_field_length);
+void add_I(char field[HEIGHT][MAX], int curr_field_length);
+void add_J(char field[HEIGHT][MAX], int curr_field_length);
+void add_K(char field[HEIGHT][MAX], int curr_field_length);
+void add_L(char field[HEIGHT][MAX], int curr_field_length);
+void add_M(char field[HEIGHT][MAX], int curr_field_length);
+void add_N(char field[HEIGHT][MAX], int curr_field_length);
+void add_O(char field[HEIGHT][MAX], int curr_field_length);
+void add_P(char field[HEIGHT][MAX], int curr_field_length);
+void add_Q(char field[HEIGHT][MAX], int curr_field_length);
+void add_R(char field[HEIGHT][MAX], int curr_field_length);
+void add_S(char field[HEIGHT][MAX], int curr_field_length);
+void add_T(char field[HEIGHT][MAX], int curr_field_length);
+void add_U(char field[HEIGHT][MAX], int curr_field_length);
+void add_V(char field[HEIGHT][MAX], int curr_field_length);
+void add_W(char field[HEIGHT][MAX], int curr_field_length);
+void add_X(char field[HEIGHT][MAX], int curr_field_length);
+void add_Y(char field[HEIGHT][MAX], int curr_field_length);
+void add_Z(char field[HEIGHT][MAX], int curr_field_length);
+void add_0(char field[HEIGHT][MAX], int curr_field_length);
+void add_1(char field[HEIGHT][MAX], int curr_field_length);
+void add_2(char field[HEIGHT][MAX], int curr_field_length);
+void add_3(char field[HEIGHT][MAX], int curr_field_length);
+void add_4(char field[HEIGHT][MAX], int curr_field_length);
+void add_5(char field[HEIGHT][MAX], int curr_field_length);
+void add_6(char field[HEIGHT][MAX], int curr_field_length);
+void add_7(char field[HEIGHT][MAX], int curr_field_length);
+void add_8(char field[HEIGHT][MAX], int curr_field_length);
+void add_9(char field[HEIGHT][MAX], int curr_field_length);
+
+#endif
